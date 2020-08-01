43871d79b0309b701585a4878425178696f24598
Oshi Agrawal <oshiag123@gmail.com>
2020-08-01 16:49:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState} from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import productsReducer from './store/reducers/product';
+import cartReducer from './store/reducers/cart';
+import ShopNavigator from './navigation/ShopNavigator';
+import orderReducer from './store/reducers/orders';
+
+const rootReducer = combineReducers({
+    products: productsReducer,
+    cart: cartReducer,
+    orders: orderReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+};
+
+
+export default function App() {
+    const [fontLoaded,setFontLoaded] = useState(false);
+
+    if (!fontLoaded) {
+        return <AppLoading startAsync={fetchFonts} onFinish={() => {
+            setFontLoaded(true)
+        }} />;
+    }
+
+
+    return (
+        <Provider store={store}>
+            <ShopNavigator />
+        </Provider>
+  );
+}
